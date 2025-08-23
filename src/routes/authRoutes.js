d79fffe20bf74b418070e6157218ed51f14890ad
Biplab Mahata <biplabmahatajk@gmail.com>
2025-08-23 11:18:54 +0530

    commit

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { login, register, logout } = require("../controllers/authController");
+const { showLogin, showRegister, login, register, logout } = require("../controllers/authController");
 
-
+router.get("/login", showLogin);
+router.get("/register", showRegister);
 router.post("/login", login);
 router.post("/register", register);
 router.get("/logout", logout);
